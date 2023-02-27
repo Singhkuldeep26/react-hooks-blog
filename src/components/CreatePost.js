62ad1c956889491daa713d4ba7b170b73323624a
kuldeep singh <pssingh1145@gmail.com>
2023-02-28 00:39:23 +0530

    Create Post,Post Details and Implement Home

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import { firestore } from '../firebase';
+import { useFormInput } from '../hooks';
 
 function CreatePost() {
-  const [title, setTitle] = useState();
-  const [subTitle, setSubTitle] = useState();
-  const [content, setContent] = useState();
+  const title = useFormInput();
+  const subTitle = useFormInput();
+  const content = useFormInput();
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -11,6 +13,13 @@ function CreatePost() {
     console.log('title', title);
     console.log('subTitle', subTitle);
     console.log('content', content);
+
+    firestore.collection('posts').add({
+      title:title.value,
+      content:content.value,
+      subTitle:subTitle.value,
+      createdAt:new Date(),
+    });
   }
 
   return (
@@ -20,22 +29,20 @@ function CreatePost() {
       <form onSubmit={handleSubmit}>
         <div className="form-field">
           <label>Title</label>
-          <input value={title} onChange={(e) => setTitle(e.target.value)} />
+          <input {...title} />
         </div>
 
         <div className="form-field">
           <label>Sub Title</label>
           <input
-            value={subTitle}
-            onChange={(e) => setSubTitle(e.target.value)}
+            {...subTitle}
           />
         </div>
 
         <div className="form-field">
           <label>Content</label>
           <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            {...content}
           ></textarea>
         </div>
 
